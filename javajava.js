90ab5e479358ace68303d560c4b76af0c4a23fcf
Geovanka <geovankakurniawan@gmail.com>
2024-09-24 20:07:33 +0700

    upload

---
+++ javajava.js
@@ -0,0 +1,34 @@
+var cardregis = document.getElementById("cardregis");
+
+function openRegister(){
+    cardregis.style.transform = "rotateY(-180deg)";
+}
+
+function openLogin(){
+    cardregis.style.transform = "rotateY(0deg)";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
